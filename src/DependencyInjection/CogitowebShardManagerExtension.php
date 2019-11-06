4f16c5821d89fe3ef58543932fd559e966de9390
Anton Chernikov <anton.chernikov@plugsurfing.com>
2019-11-06 12:03:41 +0300

    set definition

--- src/DependencyInjection/CogitowebShardManagerExtension.php
+++ src/DependencyInjection/CogitowebShardManagerExtension.php
@@ -2,7 +2,9 @@
 
 namespace Cogitoweb\ShardManagerBundle\DependencyInjection;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -17,17 +19,19 @@ class CogitowebShardManagerExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $connection = DriverManager::getConnection(array(
+        $definition = new Definition(Connection::class);
+        $definition->setFactory([DriverManager::class, 'getConnection']);
+        $definition->setArguments([
             'wrapperClass' => 'Doctrine\DBAL\Sharding\PoolingShardConnection',
             'driver' => 'pdo_mysql',
             'global' => array('user' => '', 'password' => '', 'host' => '', 'dbname' => ''),
             'shards' => array(
-              array('id' => 1, 'user' => 'slave1', 'password', 'host' => '', 'dbname' => ''),
-              array('id' => 2, 'user' => 'slave2', 'password', 'host' => '', 'dbname' => ''),
+                array('id' => 1, 'user' => 'slave1', 'password', 'host' => '', 'dbname' => ''),
+                array('id' => 2, 'user' => 'slave2', 'password', 'host' => '', 'dbname' => ''),
             ),
             'shardChoser' => 'Doctrine\DBAL\Sharding\ShardChoser\MultiTenantShardChoser',
-        ));
-        $container->set('cogitoweb.multitenant_connection', $connection);
+        ]);
+        $container->setDefinition('cogitoweb.multitenant_connection', $definition);
         
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
