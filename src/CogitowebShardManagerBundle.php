46d8712763541d496cc0447c907f28afb29bd81e
Anton Chernikov <anton.chernikov@plugsurfing.com>
2019-11-08 20:13:54 +0300

    move code to bundle

--- src/CogitowebShardManagerBundle.php
+++ src/CogitowebShardManagerBundle.php
@@ -2,6 +2,12 @@
 
 namespace Cogitoweb\ShardManagerBundle;
 
+use Cogitoweb\ShardManagerBundle\Entity\CompanyInterface;
+use Cogitoweb\ShardManagerBundle\Repository\CompanyRepositoryInterface;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -10,5 +16,41 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class CogitowebShardManagerBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     * @throws \Exception
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+        /** @var CompanyRepositoryInterface $companyRepository */
+        $companyRepository = $container->get('cogitoweb.repository.company');
+        $shards = $this->createShardsConfig($companyRepository->findOrderedById());
+
+        $definition = new Definition(Connection::class);
+        $definition->setFactory([DriverManager::class, 'getConnection']);
+        $definition->setArgument(0, [
+            'wrapperClass' => 'Doctrine\DBAL\Sharding\PoolingShardConnection',
+            'driver' => 'pdo_mysql',
+            'shards' => $shards,
+            'shardChoser' => 'Doctrine\DBAL\Sharding\ShardChoser\MultiTenantShardChoser',
+        ]);
+        $container->setDefinition('cogitoweb.multitenant_connection', $definition);
+    }
+
+    /**
+     * @param CompanyInterface[] $companies
+     * @return array
+     */
+    private function createShardsConfig(array $companies): array
+    {
+        $shards = [];
+
+        foreach ($companies as $company) {
+            $shards[] = ['id' => $company->getId(), 'url' => $company->getConfiguration()->getDatabaseConnection()];
+        }
+
+        return $shards;
+    }
 }
--- src/DependencyInjection/CogitowebShardManagerExtension.php
+++ src/DependencyInjection/CogitowebShardManagerExtension.php
@@ -2,18 +2,12 @@
 
 namespace Cogitoweb\ShardManagerBundle\DependencyInjection;
 
-use Cogitoweb\ShardManagerBundle\Entity\CompanyInterface;
-use Cogitoweb\ShardManagerBundle\Repository\CompanyRepositoryInterface;
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\DriverManager;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class CogitowebShardManagerExtension extends Extension implements CompilerPassInterface
+class CogitowebShardManagerExtension extends Extension
 {
     /**
      * @param array $configs
@@ -24,41 +18,5 @@ class CogitowebShardManagerExtension extends Extension implements CompilerPassIn
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-    }
-
-    /**
-     * @param CompanyInterface[] $companies
-     * @return array
-     */
-    public function createShardsConfig(array $companies): array
-    {
-        $shards = [];
-
-        foreach ($companies as $company) {
-            $shards[] = ['id' => $company->getId(), 'url' => $company->getConfiguration()->getDatabaseConnection()];
-        }
-
-        return $shards;
-    }
-
-    /**
-     * @param ContainerBuilder $container
-     * @throws \Exception
-     */
-    public function process(ContainerBuilder $container)
-    {
-        /** @var CompanyRepositoryInterface $companyRepository */
-        $companyRepository = $container->get('cogitoweb.repository.company');
-        $shards = $this->createShardsConfig($companyRepository->findOrderedById());
-
-        $definition = new Definition(Connection::class);
-        $definition->setFactory([DriverManager::class, 'getConnection']);
-        $definition->setArgument(0, [
-            'wrapperClass' => 'Doctrine\DBAL\Sharding\PoolingShardConnection',
-            'driver' => 'pdo_mysql',
-            'shards' => $shards,
-            'shardChoser' => 'Doctrine\DBAL\Sharding\ShardChoser\MultiTenantShardChoser',
-        ]);
-        $container->setDefinition('cogitoweb.multitenant_connection', $definition);
     }
 }
